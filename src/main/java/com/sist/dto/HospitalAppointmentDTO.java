c035aa50ef46cc198f46b8ac542408117d43b9e4
Learn0 <kim852654@gmail.com>
2021-10-29 16:40:23 +0900

    Update HospitalAppointmentDTO.java

--- src/main/java/com/sist/dto/HospitalAppointmentDTO.java
+++ src/main/java/com/sist/dto/HospitalAppointmentDTO.java
@@ -2,11 +2,6 @@ package com.sist.dto;
 
 import java.time.LocalDateTime;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class HospitalAppointmentDTO {
 
 	private Integer idx;
@@ -14,4 +9,35 @@ public class HospitalAppointmentDTO {
 	private String time;
 	private LocalDateTime reservationTime;
 	private String status;
+	
+	public Integer getIdx() {
+		return idx;
+	}
+	public void setIdx(Integer idx) {
+		this.idx = idx;
+	}
+	public Integer getMemberIdx() {
+		return memberIdx;
+	}
+	public void setMemberIdx(Integer memberIdx) {
+		this.memberIdx = memberIdx;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public LocalDateTime getReservationTime() {
+		return reservationTime;
+	}
+	public void setReservationTime(LocalDateTime reservationTime) {
+		this.reservationTime = reservationTime;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
