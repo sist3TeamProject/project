f4aee74c58390322e9f0a8dbcf9ab873f6ff2eff
WangJi <WangJi@DESKTOP-U8QE4D3>
2021-11-01 17:45:06 +0900

    응급처치

--- src/main/java/com/sist/web/TreatController.java
+++ src/main/java/com/sist/web/TreatController.java
@@ -16,11 +16,54 @@ public class TreatController {
 	@Autowired
 	private TreatDAO dao;
 	
-	@GetMapping("treat.do")
+	// 리스트
+	@GetMapping("list.do")
 	public String treat_list(Model model)
+	{
+		model.addAttribute("main_jsp", "../emergency_treat/list.jsp");
+		return "main/main";
+	}
+	
+	// 응급상황시 대처요령
+	@GetMapping("treat.do")
+	public String treat_treat(Model model)
 	{
 		model.addAttribute("main_jsp", "../emergency_treat/treat.jsp");
 		return "main/main";
 	}
+	
+	// 응급처치 개요
+	@GetMapping("treat1.do")
+	public String treat_treat1(Model model)
+	{
+		model.addAttribute("main_jsp", "../emergency_treat/treat1.jsp");
+		return "main/main";
+	}
+	
+	// 응급상황
+	@GetMapping("treat2.do")
+	public String treat_treat2(Model model)
+	{
+		model.addAttribute("main_jsp", "../emergency_treat/treat2.jsp");
+		return "main/main";
+	}
+	
+	// 기본응급처치
+	@GetMapping("treat3.do")
+	public String treat_treat3(Model model)
+	{
+		model.addAttribute("main_jsp", "../emergency_treat/treat3.jsp");
+		return "main/main";
+	}
+	
+	// 상황별 응급처치
+	@GetMapping("treat4.do")
+	public String treat_treat4(Model model)
+	{
+		model.addAttribute("main_jsp", "../emergency_treat/treat4.jsp");
+		return "main/main";
+	}
+	
+	
 
 }
