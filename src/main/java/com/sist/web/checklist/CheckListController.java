8889253a93b2d9462c74b3a97e180e4afdad30b2
WangJi <WangJi@DESKTOP-U8QE4D3>
2021-11-04 16:26:25 +0900

    111

--- src/main/java/com/sist/web/checklist/CheckListController.java
+++ src/main/java/com/sist/web/checklist/CheckListController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-@RequiredArgsConstructor
+//@Controller
+//@RequiredArgsConstructor
 public class CheckListController {
 
     private final CheckListRepository repository;
